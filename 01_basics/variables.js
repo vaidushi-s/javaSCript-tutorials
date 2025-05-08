0505bf3cb6c788a30c2b744093fd0aa3019f327d
vaidushi-s <svaidushi19@gmail.com>
2025-05-08 14:05:47 +0000

    Manual commit test

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountName = "Vaidushi"
+var accountCity = "jaipur"
+accountPassword = "12335"
+
+// accountId = 123445 not allowed as variable declared as constant cannot be changed
+
+accountName = "Miss Sharma"
+accountCity = "Bengluru"
+accountPassword = "23345"
+
+console.log(accountName)
+console.table([accountName, accountId, accountCity, accountPassword])
